99366872ae26798e73506efdab91a756935f021c
Rozhok21 <163853808+Rozhok21@users.noreply.github.com>
2024-03-19 15:08:45 +0300

    Первый комммит

---
+++ yn/yn/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library("cgjg", "gjgjgvj", 2000);
+        }
+        public class Library
+        {
+            public string Name { get; set; }
+            public string Adress { get; set; }
+            public int Year { get; set; }
+            public Library(string Name, string Adress, int Year)
+            {
+                this.Name = Name;
+                this.Adress = Adress;
+                this.Year = Year;
+
+            }
+        }
+    }
+}
